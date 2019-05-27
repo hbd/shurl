f6ad52978b75ca8cfba41d865cddf75caa2cb05e
Zachary A. Keener <zakkeener@gmail.com>
2019-05-26 19:28:40 -0500

    Update to use curl

--- main.go
+++ main.go
@@ -14,16 +14,17 @@ func main() {
 		// path := os.Getenv("PATH")
 		for {
 			var input string
-			fmt.Printf("> ")
+			fmt.Printf("curl > ")
 			scanner.Scan()
 			input = scanner.Text()
 			splitCmdLine := strings.Split(input, " ") // Split line into command and its args.
-			cmd := exec.Command(splitCmdLine[0], splitCmdLine[1:]...)
-			out, err := cmd.Output()
+			cmd := exec.Command("curl", splitCmdLine[0:]...)
+			// out, err := cmd.Output() // Contains only stdout.
+			out, err := cmd.CombinedOutput() // Contains stderr + stdout.
 			if err != nil {
 				fmt.Printf("error: %s\n", err)
 			}
-			fmt.Printf("%s", out)
+			fmt.Printf("%s\n", out)
 		}
 	}
 }
